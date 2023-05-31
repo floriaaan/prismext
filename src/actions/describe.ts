dc8335e85f6cc5eae60834e6013908b582b3edc4
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-31 20:45:22 +0200

    feat(providers): add support for postgres (discover, describe)

--- src/actions/describe.ts
+++ src/actions/describe.ts
@@ -1,25 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 
 type TableDescription = {
-  TABLE_NAME: string;
-  COLUMN_NAME: string;
-  DATA_TYPE: string;
-  IS_NULLABLE: string;
-  COLUMN_DEFAULT: string;
-  COLUMN_TYPE: string;
-  COLUMN_KEY: string;
-  EXTRA: string;
-  COLUMN_COMMENT: string;
+  table_name: string;
+  column_name: string;
+  data_type: string;
+  is_nullable: string;
+  column_default: string;
+  column_type: string;
+  column_key: string;
+  extra: string;
+  column_comment: string;
 };
 
 export const describe = async (prisma: PrismaClient, model: string) => {
-  const raw = (await prisma.$queryRaw`SELECT 
-          TABLE_NAME, COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_TYPE, COLUMN_KEY, EXTRA, COLUMN_COMMENT
+  const provider_queries = {
+    postgres: prisma.$queryRaw`SELECT 
+          table_name, column_name, data_type, is_nullable, column_default
+          FROM information_schema.columns
+          WHERE table_name = ${model} AND table_schema = 'public'`,
+    mysql: prisma.$queryRaw`SELECT 
+          TABLE_NAME as table_name, COLUMN_NAME as column_name, DATA_TYPE as data_type, IS_NULLABLE as is_nullable, COLUMN_DEFAULT as column_default, COLUMN_TYPE as column_type, COLUMN_KEY as column_key, EXTRA as extra, COLUMN_COMMENT as column_comment
           FROM INFORMATION_SCHEMA.COLUMNS
-          WHERE TABLE_NAME = ${model}`) as TableDescription[];
+          WHERE TABLE_NAME = ${model}`,
+  };
+
+  const raw = (await provider_queries["postgres"]) as TableDescription[];
 
   const description = raw?.reduce((acc, curr) => {
-    acc[curr.COLUMN_NAME] = curr;
+    acc[curr.column_name] = curr;
     return acc;
   }, {} as { [key: string]: TableDescription });
 
--- src/actions/discover.ts
+++ src/actions/discover.ts
@@ -1,16 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
 export const discover = async (prisma: PrismaClient) => {
-  const database_tables = (await prisma.$queryRaw`SELECT
-        DISTINCT(TABLE_NAME)
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_SCHEMA = DATABASE()`) as {
-    TABLE_NAME: string;
+  const provider_queries = {
+    postgres: prisma.$queryRaw`SELECT DISTINCT table_name FROM information_schema.columns WHERE table_catalog = current_database() AND table_schema = 'public'`,
+    mysql: prisma.$queryRaw`SELECT DISTINCT (TABLE_NAME as table_name) FROM information_schema.columns WHERE TABLE_SCHEMA = DATABASE()`,
+  };
+
+  const database_tables = (await provider_queries["postgres"]) as {
+    table_name: string;
   }[];
+
   const models = Object.keys(prisma).filter((key) => !key.startsWith("$") && !key.startsWith("_"));
 
   return {
-    database: database_tables.map((table) => table.TABLE_NAME).sort(),
+    database: database_tables.map((table) => table.table_name).sort(),
     models: models.sort(),
   };
 };
